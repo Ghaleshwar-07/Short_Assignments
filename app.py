94cd7c00dc66407104eb91ef5ffdf0be7defbade
Ghaleshwar Kumbar <117896970+Ghaleshwar-07@users.noreply.github.com>
2023-07-05 13:35:37 +0530

    Task 1

--- app.py
+++ app.py
@@ -1,14 +1,57 @@
-from flask import Flask
-from login_routes import login_blueprint
+from flask import Flask, request, jsonify
+from gunicorn.app.base import BaseApplication
+from multiprocessing import Process
 
 app = Flask(__name__)
-app.register_blueprint(login_blueprint)
 
-#We can also run in default Development Server by Using
-#mode= "dev"
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not is_valid_username(username):
+        return jsonify({'message': 'Invalid username'}), 400
+
+    if not is_valid_password(password):
+        return jsonify({'message': 'Invalid password'}), 400
+
+    return jsonify({'message': 'Login successful'}), 200
+
+def is_valid_username(username):
+    if not username.isalnum():
+        return False
+
+    if not 6 <= len(username) <= 12:
+        return False
+
+    return True
+
+def is_valid_password(password):
+    if len(password) < 6:
+        return False
+
+    return True
+
+class GunicornApp(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 if __name__ == '__main__':
-#    if mode == "dev":
-#        app.run(host='0.0.0.0', port=50100, debug=True)
-    app.run()
-    
+    options = {
+        'bind': '127.0.0.1:5000',
+        'workers': 4
+    }
+    GunicornApp(app, options).run()
